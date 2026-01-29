60dd745b09fe889f487bc384c172bb5896f14c4e
Zharov <zharov@proitr.ru>
2026-01-29 12:16:38 +0500

    Revert "1"
    This reverts commit 9e7ae2293f316de22ff3cc7b08066277446ae926.

--- src/main/java/ru/yandex/practicum/sleeptracker/analysis/SleepAnalysisResult.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/analysis/SleepAnalysisResult.java
@@ -1,10 +1,10 @@
 package ru.yandex.practicum.sleeptracker.analysis;
 
 public class SleepAnalysisResult {
-    final Object value;
+    final int value;
     final String description;
 
-    public SleepAnalysisResult(Object value, String description) {
+    public SleepAnalysisResult(int value, String description) {
         this.value = value;
         this.description = description;
     }
