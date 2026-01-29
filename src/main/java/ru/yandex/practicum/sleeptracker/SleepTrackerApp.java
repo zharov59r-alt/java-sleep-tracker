431d898c1b09f9dd5ebb2dffcd65011b6d32b8ff
Zharov <zharov@proitr.ru>
2026-01-29 12:30:53 +0500

    Revert "1"
    This reverts commit 0d9733aed0821d15eaf12724a5da069ebca496b8.

--- src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
@@ -1,40 +1,8 @@
 package ru.yandex.practicum.sleeptracker;
 
-import ru.yandex.practicum.sleeptracker.analysis.*;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
 public class SleepTrackerApp {
 
-    public static void main(String[] args) throws IOException {
-
-        List<Function<List<SleepingSession>, SleepAnalysisResult>> analysisProcessors = new ArrayList<>();
-        analysisProcessors.add(new Count());
-        analysisProcessors.add(new Min());
-        analysisProcessors.add(new Max());
-        analysisProcessors.add(new Avg());
-        analysisProcessors.add(new CountBAD());
-        analysisProcessors.add(new CountSleepless());
-
-
-
-        /*
-        if (args.length == 0) {
-            throw new RuntimeException("Не указан файл сессий сна");
-        }
-
-        final List<SleepingSession> sleepingSessions = SleepingSessionLoader.load(args[0]);
-*/
-        final List<SleepingSession> sleepingSessions = SleepingSessionLoader.load("sleep_log.txt");
-
-        List<SleepAnalysisResult> results = analysisProcessors.stream()
-                .map(func -> func.apply(sleepingSessions))
-                .peek(System.out::println)
-                .collect(Collectors.toList());
+    public static void main(String[] args) {
 
     }
 }
